065988c6b02296a79ec7f266a2c3e8924819145e
yusti <SENA@CARAPRCTIFSD482.FormacionSena.RED>
2024-12-13 08:35:49 -0500

    Back listo puta

--- Controllers/Usuarios.php
+++ Controllers/Usuarios.php
@@ -5,10 +5,6 @@ class Usuarios extends Controllers
     public function __construct()
     {
         parent::__construct();
-        session_start();
-       /*  if(empty($_SESSION['login'])){
-            header('Location: ' . base_url().'/login' );
-        } */
     }
     public function usuarios()
     {

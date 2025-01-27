66a504d14a52c5f37b7a8fc7669fe57aa3b7a9fb
NicolasM <nicolaschompi@gmail.com>
2025-01-27 15:04:23 -0500

    Modificacion SQL

--- inasistencias.sql
+++ inasistencias.sql
@@ -332,6 +332,40 @@ ALTER TABLE `usuario_has_ficha`
   ADD CONSTRAINT `fk_Usuario_has_Ficha_Usuario` FOREIGN KEY (`usuario_idUsuarios`) REFERENCES `usuario` (`idUsuarios`);
 COMMIT;
 
+--
+--  Eventos 'registrar inasistencias excluyendo a los que cuentas con excepciones'
+--
+
+CREATE EVENT registrar_inasistencias
+ON SCHEDULE EVERY 1 DAY 
+STARTS '2025-01-25 05:40:00' 
+DO
+BEGIN
+   
+    INSERT INTO inasistencias (fecha, hora, codigoNovedad, usuario_idUsuarios, idInstructor, status)
+    SELECT 
+        CURDATE() AS fecha,               
+        CURTIME() AS hora,                
+        1 AS codigoNovedad,               
+        u.idUsuarios AS usuario_idUsuarios,
+        NULL AS idInstructor,             
+        1 AS status                       
+    FROM 
+        usuario u
+    LEFT JOIN excepciones e
+        ON (
+            (e.usuarioId = u.idUsuarios AND CURDATE() BETWEEN DATE(e.fechaInicio) AND DATE(e.fechaFin)) 
+            OR 
+            (e.fichaId IN (SELECT ficha_idFicha FROM usuario_has_ficha WHERE usuario_idUsuarios = u.idUsuarios) AND CURDATE() BETWEEN DATE(e.fechaInicio) AND DATE(e.fechaFin)) 
+            OR
+            (e.usuarioId IS NULL AND e.fichaId IS NULL AND CURDATE() BETWEEN DATE(e.fechaInicio) AND DATE(e.fechaFin))
+        )
+    WHERE 
+        u.rol = 'APRENDIZ'                
+        AND u.status = 1                  
+        AND e.idExcepciones IS NULL;      
+END;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

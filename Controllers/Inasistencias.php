32039ed687febdf090d9f1882a83cf29bc39c784
Sebas <sebaslg96@gmail.com>
2025-06-24 16:29:30 -0500

    arreglado inasistencias

--- Controllers/Inasistencias.php
+++ Controllers/Inasistencias.php
@@ -1,37 +1,5 @@
 <?php 
 
-class Inasistencias extends Controllers{
-    public function __construct(){
-        parent::__construct();
-    }
-    public function inasistencias(){
-
-
-        $data['page_title'] = "Página de inasistencias";
-        $data['page_name'] = "inasistencias";
-        $data['script'] = "inasistencias";
-        $this->views->getView($this,"inasistencias", $data);
-    }
-
-    public function setInasistencia($codigo){
-        $srtCodigo = strClean($codigo);
-        
-        $usuarioEncontrado = $this->model->selectCodigo($srtCodigo);
-
-        if (!empty($usuarioEncontrado)){
-            $registro = $this->model->insertInasistencia($usuarioEncontrado['idUsuarios']);
-            if ($registro) {
-                $arrResponse = array('status' => true, 'msg' => 'Se registró la inasistencia de ' . $usuarioEncontrado['nombre'] . ' ' . $usuarioEncontrado['apellido']);
-            }else{
-                $arrResponse = array('status' => false, 'msg' => 'Error al intentar insertar la asistencia de ' . $usuarioEncontrado['nombre'] . ' ' . $usuarioEncontrado['apellido']);
-            }
-        }else{
-            $arrResponse = array('status' => false, 'msg' => 'No se encontró ningún usuario con ese código');
-        }
-        echo json_encode($arrResponse, JSON_UNESCAPED_UNICODE);
-    }
-}<?php
-
 class Inasistencias extends Controllers
 {
     public function __construct()
